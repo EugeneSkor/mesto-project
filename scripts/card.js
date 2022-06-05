b07837598b10f4cd9f0f423b652637fd94b8cba6
Evgeniy Skorobogatov <evgeniyskorobog@yandex.ru>
2022-06-05 22:58:38 +0300

    add class Card

--- scripts/card.js
+++ scripts/card.js
@@ -1,5 +1,73 @@
-import Card from "./card.js";
-import { initialCards } from "./cards.js"
+export default class Card {
+  constructor(cardData, templateSelector) {
+    this._templateSelector = templateSelector
+    this._caption = data.name;
+    this._imageLink = data.link;
+    this._imageAlt = data.alt;
+    this._options = options;
+    this._template = this._getTemplateElement(options.template);
+    this._element = null;
+    this._elementCaption = null;
+    this._elementImage = null;
+    this._elementLike = null;
+    this._elementRemove = null;
+  }
+
+  _getTemplateElement(cardSelector) {
+    return document.querySelector(templateSelector).content.querySelector(this._options.classCard);
+  }
+
+  _openImage() {
+    openImage({
+      link: this._imageLink,
+      alt: this._imageAlt,
+      name: this._caption,
+    });
+  }
+
+  _toggleLike() {
+    this._elementLike.classList.toggle(this._options.likeButtonActive);
+  }
+
+  _removeCard() {
+    this._element.remove();
+    this._element = null;
+    this._elementCaption = null;
+    this._elementImage = null;
+    this._elementLike = null;
+    this._elementRemove = null;
+  }
+
+  _setEventListeners() {
+    this._elementImage.addEventListener("click", () => this._openImage());
+    this._elementLike.addEventListener("click", () => this._toggleLike());
+    this._elementRemove.addEventListener("click", () => this._removeCard());
+  }
+
+  _getCardTemplate() {
+    const cardElement = document.querySelector('.element').content.querySelector('.card').cloneNode(true);
+
+    return cardElement;
+  }
+
+
+  createCard() {
+    this._element = this._template.cloneNode(true);
+    this._elementCaption = this._element.querySelector(this._options.caption);
+    this._elementImage = this._element.querySelector(this._options.image);
+    this._elementLike = this._element.querySelector(this._options.likeButton);
+    this._elementRemove = this._element.querySelector(this._options.removeButton);
+
+    this._elementCaption.textContent = this._caption;
+    this._elementImage.src = this._imageLink;
+    this._elementImage.alt = this._imageAlt;
+
+    this._setEventListeners();
+
+    return this._element;
+  }
+}
+
 
 const reverseInitialCards = initialCards.reverse();
 
@@ -43,14 +111,14 @@ const submitaddCardButton = newCardformElement.querySelector('.form__button');
 
 // For photo popup
 
-export const popupCardPhoto = page.querySelector('#popupCardPhoto');
+const popupCardPhoto = page.querySelector('#popupCardPhoto');
 const popupPhotoframe = popupCardPhoto.querySelector('.popup__photoframe');
-export const popupPhoto = popupPhotoframe.querySelector('.popup__photo');
-export const popupDescription = popupPhotoframe.querySelector('.popup__description');
+const popupPhoto = popupPhotoframe.querySelector('.popup__photo');
+const popupDescription = popupPhotoframe.querySelector('.popup__description');
 
 // Popup functions
 
-export function openPopup (popup) {
+function openPopup (popup) {
   document.addEventListener('keydown', closeByEsc);
   document.addEventListener('click', closeByOverlayClick);
   popup.classList.add('popup_opened');
@@ -89,7 +157,7 @@ function submitUserForm (evt) {
   closePopup(profilePopup);
 }
 
-/* const createCard = (card) => {
+const createCard = (card) => {
   // клонируем содержимое тега template
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
@@ -120,11 +188,11 @@ function submitUserForm (evt) {
 
   return(cardElement)
 
-}; */
+};
 
 const renderCard = (initialCards) => {
   initialCards.forEach((item) => {
-    cardsContainer.prepend(Card.generateCard(item));
+    cardsContainer.prepend(createCard(item));
   })
 }
 
